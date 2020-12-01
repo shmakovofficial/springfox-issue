c77b0433f4b9c83243470860a71170d070bd4994
shmakovofficial <probablyviolin@gmail.com>
2020-12-01 12:58:06 +0300

    Init

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @ApiOperation(value = "getInt", httpMethod = "POST")
+    @RequestMapping
+    public int getInt() {
+        return 0;
+    }
+
+}
